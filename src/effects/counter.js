0026ab3b45c925acfe21f09a70e0872def1c090d
selfup <boudinot.regis@yahoo.com>
2017-02-03 09:24:47 -0700

    add subs ands effects directories to promote proper structure

---
+++ src/effects/counter.js
@@ -0,0 +1,5 @@
+/**
+ * There are no effects for this application
+ * But this is an example of directory and file structure for effects
+ * Seperation of concerns are key in functional paradigms!
+ */
