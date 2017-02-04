1923813e5f430bb70f4df25b79bb0fc022504aa8
selfup <boudinot.regis@yahoo.com>
2017-02-04 13:46:22 -0700

    change html a bit - add css loader = very basic styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    './src/index.js',
+    './styles/app.css',
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1',
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './bundle.css',
+      allChunks: true,
+    }),
+  ],
 };
