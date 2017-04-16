ebac137eeb7654117885a64d9cda356df5c92566
Regis <boudinot.regis@yahoo.com>
2017-04-16 16:37:18 -0600

    rename hooks to events

---
+++ src/events/index.js
@@ -0,0 +1,5 @@
+/**
+ * There are no events for this application
+ * But this is an example of directory and file structure for events
+ * Seperation of concerns are key in functional paradigms!
+*/
