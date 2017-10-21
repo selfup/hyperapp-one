c71dd504fd0b1e8595db79dfb03e6870ef25ee52
Regis <boudinot.regis@yahoo.com>
2017-10-21 00:23:59 -0600

    revert ViewFunctions back to components - leave in eslint fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { app } from 'hyperapp';
 import actions from './actions';
 import state from './state';
-import view from './ViewFunctions/Counter';
+import view from './components/Counter';
 
 const dispatch = app({
   state,
