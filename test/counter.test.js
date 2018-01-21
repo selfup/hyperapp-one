735e161603eb4fc3637de08892003375c30f1163
selfup <boudinot.regis@yahoo.com>
2018-01-21 14:39:51 -0600

    add comments to test files

--- test/counter.test.js
+++ test/counter.test.js
@@ -3,6 +3,10 @@ import appActions from './../src/actions';
 import appState from './../src/state';
 import Counter from './../src/components/Counter';
 
+// This is not a static component so we inject it into an app and test
+// This is a unit test and we are testing actual clicks
+// We are simply testing via exposed actions
+// And checking the DOM for state changes
 beforeEach(() => {
   document.body.innerHTML = '';
 });
--- test/description.test.js
+++ test/description.test.js
@@ -1,5 +1,6 @@
 import Description from './../src/components/Description';
 
+// Because this is a static component we just verify the results of h calls
 test('Description component renders', () => {
   expect(Description()).toEqual({
     children: [{
