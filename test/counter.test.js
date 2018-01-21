978537b1f04c24405dbbb2da827ae505f7cfc04a
selfup <boudinot.regis@yahoo.com>
2018-01-21 15:24:58 -0600

    update Counter test comments

--- test/counter.test.js
+++ test/counter.test.js
@@ -3,8 +3,8 @@ import appActions from './../src/actions';
 import appState from './../src/state';
 import Counter from './../src/components/Counter';
 
-// This is not a static component so we inject it into an app and test
-// This is a unit test and we are testing actual clicks
+// This is not a static component so we inject it into an app and test it
+// This is a unit test and we are not testing actual clicks
 // We are simply testing via exposed actions
 // And checking the DOM for state changes
 beforeEach(() => {
