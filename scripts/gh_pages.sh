9655dca3fccb1e4e9958422f16837245b3f7756f
Regis <selfup@protonmail.com>
2018-06-16 16:50:22 -0500

    keep build dir

--- scripts/gh_pages.sh
+++ scripts/gh_pages.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 set -x
 
-git subtree split --prefix build -b gh-pages
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
-rm -rf build
+git push origin :gh-pages \
+  && git subtree push --prefix build origin gh-pages \
+  && git branch -D gh-pages \
+  && rm -rf build
