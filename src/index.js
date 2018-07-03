4bc690966a7a198415e934214793ab6cca259cc5
Regis <boudinot.regis@yahoo.com>
2018-07-03 17:58:25 -0500

    add redux devtools, fix eslint nightmare for dynamic import

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
 import { app } from 'hyperapp';
+
 import actions from './actions';
 import state from './state';
 import view from './components/Counter';
 
-const {
-  add,
-  sub,
-} = app(
+const appArgs = [
   state,
   actions,
   view,
-  document.body,
-);
+  document.getElementById('app'),
+];
 
-/**
- * Hyperapp wires your actions so the view is re-rendered every time the state
- * changes as a result of calling any action. This object is useful because it
- * allows you to talk to your app from another app, framework, vanilla JS, etc.
- *
- * Here is an example on CodePen: https://codepen.io/selfup/pen/jLMRjO
- */
+function onMount(main) {
+  const {
+    add,
+    sub,
+  } = main;
 
-setTimeout(add, 1000);
-setTimeout(sub, 2000);
+  /**
+   * Hyperapp wires your actions so the view is re-rendered every time the state
+   * changes as a result of calling any action. This object is useful because it
+   * allows you to talk to your app from another app, framework, vanilla JS, etc.
+   *
+   * Here is an example on CodePen: https://codepen.io/selfup/pen/jLMRjO
+   */
+
+  setTimeout(add, 1000);
+  setTimeout(sub, 2000);
+}
+
+let main;
+
+if (process.env.NODE_ENV !== 'production') {
+  import('hyperapp-redux-devtools')
+    .then((devtools) => {
+      main = devtools(app)(...appArgs);
+
+      onMount(main);
+    });
+} else {
+  main = app(...appArgs);
+
+  onMount(main);
+}
