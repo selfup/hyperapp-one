e89e777e24eb17553cac84575cb56d65007eb5cf
selfup <boudinot.regis@yahoo.com>
2019-02-28 21:52:24 -0600

    add airbnb safe prettier config and update files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,20 @@
 module.exports = {
-  'extends': 'airbnb-base',
-  'plugins': [
-    'import',
-    'react',
-    'jest',
-  ],
-  'rules': {
+  extends: 'airbnb-base',
+  plugins: ['import', 'react', 'jest'],
+  rules: {
     'linebreak-style': 'off',
-    'no-unused-vars': [2, { 'varsIgnorePattern': 'h' }],
+    'no-unused-vars': [2, { varsIgnorePattern: 'h' }],
     'react/jsx-uses-vars': 2,
   },
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'allowImportExportEverywhere': true,
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-  'env': {
-    'browser': true,
-    'jest': true,
-  }
+  env: {
+    browser: true,
+    jest: true,
+  },
 };
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,8 +1,11 @@
 import { h } from 'hyperapp';
 
-export default () =>
+export default () => (
   <div>
     <h1>hyperapp-one</h1>
-    <p><em>With JSX and Webpack</em></p>
+    <p>
+      <em>With JSX and Webpack</em>
+    </p>
     <hr />
-  </div>;
+  </div>
+);
