796c0a1798527490104386a0d2afaa22dd4dee6b
selfup <boudinot.regis@yahoo.com>
2019-08-02 18:19:52 -0500

    comments to v2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,6 @@ import { add, sub } from './../actions';
 
 /**
  * first object in the store is 'state' (an object - {})
- * second object in the store is 'actions' (an object - {})
  * here we destructure what is needed
  * 'num' from 'state' and 'add'/'sub' from 'actions'
  */
